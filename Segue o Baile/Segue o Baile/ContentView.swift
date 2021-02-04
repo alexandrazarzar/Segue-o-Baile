b9a809822fa52a37a785d8143335f9e2cf002867
Alexandra Zarzar <alexandra@MacBook-Pro-de-Alexandra.local>
2021-02-04 17:19:01 -0300

    adiciona projeto do xcode

---
+++ Segue o Baile/Segue o Baile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Segue o Baile
+//
+//  Created by Alexandra Zarzar on 04/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
