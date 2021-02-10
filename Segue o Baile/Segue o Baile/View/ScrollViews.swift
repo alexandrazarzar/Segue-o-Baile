ac90d7c7d6477d27e18ce80b5ca779528fe8e968
Meyrillan <meyrillan@gmail.com>
2021-02-10 15:53:00 -0300

    Adiciona scrollviews e estilizações

--- Segue o Baile/Segue o Baile/View/ScrollViews.swift
+++ Segue o Baile/Segue o Baile/View/ScrollViews.swift
@@ -16,12 +16,13 @@ struct ScrollViews: View {
 struct CategoriesScroll: View {
     @State var dados = Data.init()
     @State var categorySelected: Int = 1
+    @State var costumeSelected: Int = -1
     
     var body: some View {
         
         VStack {
             
-            CostumesScroll(dados: $dados, categorySelected: $categorySelected)
+            CostumesScroll(dados: $dados, categorySelected: $categorySelected, costumeSelected: $costumeSelected )
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
@@ -55,15 +56,47 @@ struct CostumesScroll: View {
     
     @Binding var dados: Data
     @Binding var categorySelected: Int
+    @Binding var costumeSelected: Int
     
     var body: some View {
-        ScrollView(.horizontal) {
+        ScrollView(.horizontal, showsIndicators: false) {
             HStack {
                 ForEach(0..<dados.categories[categorySelected].costume.count, id:\.self) { count in
                     Button(action: {
                         
+                        costumeSelected = count
+                        
                     }, label: {
                         
+                        ZStack {
+                            
+                            if costumeSelected != count {
+                             
+                                Circle()
+                                    .foregroundColor(.white)
+                                    .frame(width: 48, height: 48)
+                                
+                                Image(dados.categories[categorySelected].costume[count].imageCostume!)
+                                    .resizable()
+                                    .frame(width: 32, height: 32)
+                                    .padding()
+                                
+                            } else {
+                                
+                                Circle().stroke(Color.blue, lineWidth: 3)
+                                    .foregroundColor(.black)
+                                    .frame(width: 60, height: 60)
+                                
+                                Image(dados.categories[categorySelected].costume[count].imageCostume!)
+                                    .resizable()
+                                    .frame(width: 36, height: 36)
+                                    .padding()
+                                
+                            }
+                            
+                        }
+                        
+                        .padding(.leading)
                     })
                 }
             }
